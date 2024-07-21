46b0344cc51e68b529fd7cea82546626b8a21289
Shack <g0972222165@gmail.com>
2024-07-21 12:03:23 +0800

    Add Form

--- item/models.py
+++ item/models.py
@@ -2,27 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-      name=models.CharField(max_length=260)
-      
-      class Meta:
-            ordering=('name',)
-            verbose_name_plural='種類分類'
-      
-      def __str__(self):
-            return self.name 
+    name = models.CharField(max_length=260)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = '種類分類'
+
+    def __str__(self):
+        return self.name 
 
 class Items(models.Model):
-      name=models.CharField(max_length=260)
-      category=models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
-      description=models.TextField(blank=True,null=True)
-      price=models.FloatField()
-      image=models.ImageField(upload_to='item_images',blank=True,null=True)
-      is_sold=models.BooleanField(default=False)
-      crated_by=models.ForeignKey(User,related_name='items',on_delete=models.CASCADE)
-      created_at=models.DateTimeField(auto_now_add=True)
-     
-      
-      def __str__(self):
-            return self.name 
-      class Meta:
-            verbose_name_plural='項目'
+    name = models.CharField(max_length=260)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)  # 修改這裡
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name 
+
+    class Meta:
+        verbose_name_plural = '項目'
--- item/views.py
+++ item/views.py
@@ -3,6 +3,8 @@ from .models import Items
 
 def details(request,pk):
       item=get_object_or_404(Items,pk=pk)
+      related_items=Items.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
       return render(request,'item/detail.html',{
-            'item':item
+            'item':item,
+            'related_items':related_items
       })
